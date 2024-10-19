0730c40666d24be78851a126e2717cda8798e0b6
hibah04 <149254154+hibah04@users.noreply.github.com>
2024-10-19 17:35:21 +0100

    Update codesofar.py

--- codesofar.py
+++ codesofar.py
@@ -5,17 +5,24 @@ data = pd.read_csv('HomelessPeople.csv')
 
 def splitofmoney (x):
     
-    a,b = 0.4,0.6
+    a,b=0.4,0.6
 
     weights = {
         'homeless_people': 0.6,
-        'meanaffordability': 0.4,
+        #'meanaffordability': 0.4,
         'noofshelters': a,
         'foodbanks': 0.6
     }
 
     weightings = {
         data['homeless_people']*weights['homeless_people'],
-        data['meanaffordability']*weights['meanaffordability']
+        #data['meanaffordability']*weights['meanaffordability']
     }
-
+    
+    #Calculating the total number of homeless people in total in London
+    total_homeless_people = data['homeless_people'].sum()
+    
+    #Create a new column in data frame that has proportion of homeless ppl in each borough of the total number of homeless ppl
+    data['Proportion that is homeless'] = (data['homeless_people'] / total_homeless_people) * 100
+    
+    
